e5d5aece9dcec556414e8c0d33268dbe54de42c0
Charles Ciampa <ceciampa@gmail.com>
2024-04-16 17:16:16 -0400

    Replacing agent with code from ex8

--- agent.py
+++ agent.py
@@ -1,83 +1,430 @@
-import random
-import tensorflow as tf
-from tensorflow.keras import layers, models, optimizers
+from collections import namedtuple
+import copy
 import numpy as np
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import tqdm
 
-class DQNAgent:
-    def __init__(self, state_shape, action_size, learning_rate=0.001, gamma=0.99, epsilon=1.0, epsilon_min=0.01, epsilon_decay=0.995):
-        self.state_shape = state_shape
-        self.action_size = action_size
-        self.learning_rate = learning_rate
-        self.gamma = gamma 
-        self.epsilon = epsilon 
-        self.epsilon_min = epsilon_min
-        self.epsilon_decay = epsilon_decay
-        self.memory = []
-        self.model = self.build_model()
-        self.target_model = self.build_model()
-        self.update_target_model()
+# Batch namedtuple, i.e. a class which contains the given attributes
+Batch = namedtuple("Batch", ("states", "actions", "rewards", "next_states", "dones"))
 
-    # Define the neural network model
-    def build_model(self):
-        model = models.Sequential([
-            layers.Dense(24, input_shape=self.state_shape, activation='relu'),
-            layers.Dense(24, activation='relu'),
-            layers.Dense(self.action_size, activation='linear')
-        ])
-        model.compile(loss='mse', optimizer=optimizers.Adam(learning_rate=self.learning_rate))
+
+class ReplayMemory:
+    def __init__(self, max_size, state_size):
+        """Replay memory implemented as a circular buffer.
+
+        Experiences will be removed in a FIFO manner after reaching maximum
+        buffer size.
+
+        Args:
+            - max_size: Maximum size of the buffer
+            - state_size: Size of the state-space features for the environment
+        """
+        self.max_size = max_size
+        self.state_size = state_size
+
+        # Preallocating all the required memory, for speed concerns
+        self.states = torch.empty((max_size, state_size))
+        self.actions = torch.empty((max_size, 1), dtype=torch.long)
+        self.rewards = torch.empty((max_size, 1))
+        self.next_states = torch.empty((max_size, state_size))
+        self.dones = torch.empty((max_size, 1), dtype=torch.bool)
+
+        # Pointer to the current location in the circular buffer
+        self.idx = 0
+        # Indicates number of transitions currently stored in the buffer
+        self.size = 0
+
+    def add(self, state, action, reward, next_state, done):
+        """Add a transition to the buffer.
+
+        :param state: 1-D np.ndarray of state-features
+        :param action: Integer action
+        :param reward: Float reward
+        :param next_state: 1-D np.ndarray of state-features
+        :param done: Boolean value indicating the end of an episode
+        """
+
+        # YOUR CODE HERE: Store the input values into the appropriate
+        # attributes, using the current buffer position `self.idx`
+
+        self.states[self.idx] = torch.from_numpy(state)
+        self.actions[self.idx] = action
+        self.rewards[self.idx] = reward
+        self.next_states[self.idx] = torch.from_numpy(next_state)
+        self.dones[self.idx] = done
+
+        # DO NOT EDIT
+        # Circulate the pointer to the next position
+        self.idx = (self.idx + 1) % self.max_size
+        # Update the current buffer size
+        self.size = min(self.size + 1, self.max_size)
+
+    def sample(self, batch_size) -> Batch:
+        """Sample a batch of experiences.
+
+        If the buffer contains less that `batch_size` transitions, sample all
+        of them.
+
+        :param batch_size: Number of transitions to sample
+        :rtype: Batch
+        """
+
+        # YOUR CODE HERE: Randomly sample an appropriate number of
+        # transitions *without replacement*. If the buffer contains less than
+        # `batch_size` transitions, return all of them. The return type must
+        # be a `Batch`.
+
+        if self.size < batch_size:
+            return Batch(
+                states=self.states[: self.size],
+                actions=self.actions[: self.size],
+                rewards=self.rewards[: self.size],
+                next_states=self.next_states[: self.size],
+                dones=self.dones[: self.size],
+            )
+
+        sample_indices = np.random.choice(self.size, batch_size, replace=False)
+        batch = Batch(
+            states=self.states[sample_indices],
+            actions=self.actions[sample_indices],
+            rewards=self.rewards[sample_indices],
+            next_states=self.next_states[sample_indices],
+            dones=self.dones[sample_indices],
+        )
+
+        return batch
+
+    def populate(self, env, num_steps):
+        """Populate this replay memory with `num_steps` from the random policy.
+
+        :param env: Gymnasium environment
+        :param num_steps: Number of steps to populate the replay memory
+        """
+
+        # YOUR CODE HERE: Run a random policy for `num_steps` time-steps and
+        # populate the replay memory with the resulting transitions.
+        # Hint: Use the self.add() method.
+
+        state, _ = env.reset()
+        for _ in range(num_steps):
+            action = env.action_space.sample()
+            next_state, reward, done, _, _ = env.step(action)
+            self.add(state, action, reward, next_state, done)
+            if done:
+                state, _ = env.reset()
+            else:
+                state = next_state
+
+
+class DQN(nn.Module):
+    def __init__(self, state_dim, action_dim, *, num_layers=20, hidden_dim=256):
+        """Deep Q-Network PyTorch model.
+
+        Args:
+            - state_dim: Dimensionality of states
+            - action_dim: Dimensionality of actions
+            - num_layers: Number of total linear layers
+            - hidden_dim: Number of neurons in the hidden layers
+        """
+
+        super().__init__()
+        self.state_dim = state_dim
+        self.action_dim = action_dim
+        self.num_layers = num_layers
+        self.hidden_dim = hidden_dim
+
+        # YOUR CODE HERE: Define the layers of your model such that
+        # * there are `num_layers` nn.Linear modules / layers
+        # * all activations except the last should be ReLU activations
+        #   (this can be achieved either using a nn.ReLU() object or the nn.functional.relu() method)
+        # * the last activation can either be missing, or you can use nn.Identity()
+        # Hint: A regular Python list of layers is tempting, but PyTorch does not register
+        # these parameters in its computation graph. See nn.ModuleList or nn.Sequential
+        self.in_layer = nn.Linear(self.state_dim, self.hidden_dim)
+        self.hidden_layers = nn.ModuleList(
+            [nn.Linear(self.hidden_dim, self.hidden_dim), nn.ReLU()]
+            * (self.num_layers - 1)
+        )
+        self.final_hidden_layer = nn.Linear(self.hidden_dim, self.action_dim)
+
+    def forward(self, states) -> torch.Tensor:
+        """Q function mapping from states to action-values.
+
+        :param states: (*, S) torch.Tensor where * is any number of additional
+                dimensions, and S is the dimensionality of state-space
+        :rtype: (*, A) torch.Tensor where * is the same number of additional
+                dimensions as the `states`, and A is the dimensionality of the
+                action-space. This represents the Q values Q(s, .)
+        """
+        # YOUR CODE HERE: Use the defined layers and activations to compute
+        # the action-values tensor associated with the input states.
+        # Hint: Do not worry about the * arguments above (previous dims in tensor).
+        # PyTorch functions typically handle those properly.
+
+        x = self.in_layer(states)
+        x = F.relu(x)
+        for layer in self.hidden_layers:
+            x = layer(x)
+        x = self.final_hidden_layer(x)
+        x = F.relu(x)
+        x = nn.Identity()(x)
+        return x
+
+    # DO NOT EDIT: Utility methods for cloning and storing models.
+
+    @classmethod
+    def custom_load(cls, data):
+        model = cls(*data["args"], **data["kwargs"])
+        model.load_state_dict(data["state_dict"])
         return model
 
-    # Update the target model with the weights of the main model
-    def update_target_model(self):
-        self.target_model.set_weights(self.model.get_weights())
+    def custom_dump(self):
+        return {
+            "args": (self.state_dim, self.action_dim),
+            "kwargs": {
+                "num_layers": self.num_layers,
+                "hidden_dim": self.hidden_dim,
+            },
+            "state_dict": self.state_dict(),
+        }
 
-    # Store the state, action, reward, next_state, and done in the memory
-    def remember(self, state, action, reward, next_state, done):
-        self.memory.append((state, action, reward, next_state, done))
 
-    # Choose an action based on the epsilon-greedy policy
-    def choose_action(self, state):
-        if np.random.rand() <= self.epsilon:
-            return np.random.randint(self.action_size)
-        q_values = self.model.predict(np.expand_dims(state, axis=0))
-        return np.argmax(q_values[0])
+# DO NOT EDIT: Test code
 
-    # Train the model using the experience replay technique
-    def replay(self, batch_size):
-        if len(self.memory) < batch_size:
-            return
-        
-        batch = random.sample(self.memory, batch_size)
-        for state, action, reward, next_state, done in batch:
-            try:
-                # Preprocess state
-                processed_state = np.array(state)
-                processed_next_state = np.array(next_state)
-            except ValueError:
-                processed_state = [np.array(sub_state) for sub_state in state]
-                processed_next_state = [np.array(sub_next_state) for sub_next_state in next_state]
 
-            target = reward
-            if not done:
-                target = reward + self.gamma * np.amax(self.target_model.predict(processed_next_state)[0])
+def _test_dqn_forward(dqn_model, input_shape, output_shape):
+    """Tests that the dqn returns the correctly shaped tensors."""
+    inputs = torch.torch.randn((input_shape))
+    outputs = dqn_model(inputs)
 
-            target_f = self.model.predict(processed_state)
-            target_f[0][action] = target
-            self.model.fit(processed_state, target_f, epochs=1, verbose=0)
+    if not isinstance(outputs, torch.FloatTensor):
+        raise Exception(
+            f"DQN.forward returned type {type(outputs)} instead of torch.Tensor"
+        )
 
-        if self.epsilon > self.epsilon_min:
-            self.epsilon *= self.epsilon_decay
+    if outputs.shape != output_shape:
+        raise Exception(
+            f"DQN.forward returned tensor with shape {outputs.shape} instead of {output_shape}"
+        )
 
-    # Train the model for a given number of episodes
-    def train(self, env, episodes, batch_size=32):
-        for episode in range(episodes):
-            state = env.reset()
-            done = False
-            while not done:
-                action = self.choose_action(state)
-                next_state, reward, done, _ = env.step(action)
-                self.remember(state, action, reward, next_state, done)
-                state = next_state
-                self.replay(batch_size)
-            self.update_target_model()
-            print(f"Episode: {episode + 1}, Epsilon: {self.epsilon}")
+    if not outputs.requires_grad:
+        raise Exception(
+            f"DQN.forward returned tensor which does not require a gradient (but it should)"
+        )
+
+
+dqn_model = DQN(10, 4)
+_test_dqn_forward(dqn_model, (64, 10), (64, 4))
+_test_dqn_forward(dqn_model, (2, 3, 10), (2, 3, 4))
+del dqn_model
+
+dqn_model = DQN(64, 16)
+_test_dqn_forward(dqn_model, (64, 64), (64, 16))
+_test_dqn_forward(dqn_model, (2, 3, 64), (2, 3, 16))
+del dqn_model
+
+# Testing custom dump / load
+dqn1 = DQN(10, 4, num_layers=10, hidden_dim=20)
+dqn2 = DQN.custom_load(dqn1.custom_dump())
+assert dqn2.state_dim == 10
+assert dqn2.action_dim == 4
+assert dqn2.num_layers == 10
+assert dqn2.hidden_dim == 20
+
+
+def train_dqn_batch(optimizer, batch, dqn_model, dqn_target, gamma) -> float:
+    """Perform a single batch-update step on the given DQN model.
+
+    :param optimizer: nn.optim.Optimizer instance
+    :param batch: Batch of experiences (class defined earlier)
+    :param dqn_model: The DQN model to be trained
+    :param dqn_target: The target DQN model, ~NOT~ to be trained
+    :param gamma: The discount factor
+    :rtype: Float. The scalar loss associated with this batch
+    """
+    # YOUR CODE HERE: Compute the values and target_values tensors using the
+    # given models and the batch of data.
+    # Recall that 'Batch' is a named tuple consisting of
+    # ('states', 'actions', 'rewards', 'next_states', 'dones')
+    # Hint: Remember that we should not pass gradients through the target network
+    # dqn_model.train()
+    values = dqn_model(batch.states).gather(1, batch.actions)
+    target_values = batch.rewards + gamma * torch.max(
+        dqn_target(batch.next_states).detach(), dim=1
+    ).values.unsqueeze(1) * (1 - batch.dones.int())
+
+    # DO NOT EDIT
+
+    assert (
+        values.shape == target_values.shape
+    ), "Shapes of values tensor and target_values tensor do not match."
+
+    # Testing that the values tensor requires a gradient,
+    # and the target_values tensor does not
+    assert values.requires_grad, "values tensor requires gradients"
+    assert (
+        not target_values.requires_grad
+    ), "target_values tensor should not require gradients"
+
+    # Computing the scalar MSE loss between computed values and the TD-target
+    # DQN originally used Huber loss, which is less sensitive to outliers
+    loss = F.mse_loss(values, target_values)
+
+    optimizer.zero_grad()  # Reset all previous gradients
+    loss.backward()  # Compute new gradients
+    optimizer.step()  # Perform one gradient-descent step
+
+    return loss.item()
+
+
+def train_dqn(
+    env,
+    num_steps,
+    model,
+    *,
+    num_saves=5,
+    replay_size,
+    replay_prepopulate_steps=0,
+    batch_size,
+    exploration,
+    gamma,
+):
+    """
+    DQN algorithm.
+
+    Compared to previous training procedures, we will train for a given number
+    of time-steps rather than a given number of episodes. The number of
+    time-steps will be in the range of millions, which still results in many
+    episodes being executed.
+
+    Args:
+        - env: The Gymnasium environment
+        - num_steps: Total number of steps to be used for training
+        - num_saves: How many models to save to analyze the training progress
+        - replay_size: Maximum size of the ReplayMemory
+        - replay_prepopulate_steps: Number of steps with which to prepopulate
+                                    the memory
+        - batch_size: Number of experiences in a batch
+        - exploration: An ExponentialSchedule
+        - gamma: The discount factor
+
+    Returns: (saved_models, returns)
+        - saved_models: Dictionary whose values are trained DQN models
+        - returns: Numpy array containing the return of each training episode
+        - lengths: Numpy array containing the length of each training episode
+        - losses: Numpy array containing the loss of each training batch
+    """
+    # Check that environment states are compatible with our DQN representation
+    assert (
+        isinstance(env.observation_space, gym.spaces.Box)
+        and len(env.observation_space.shape) == 1
+    )
+
+    # Get the state_size from the environment
+    state_size = env.observation_space.shape[0]
+
+    # Initialize the DQN and DQN-target models
+    dqn_model = model(state_size, env.action_space.n)
+    dqn_target = model.custom_load(dqn_model.custom_dump())
+
+    # Initialize the optimizer
+    optimizer = torch.optim.Adam(dqn_model.parameters())
+
+    # Initialize the replay memory and prepopulate it
+    memory = ReplayMemory(replay_size, state_size)
+    memory.populate(env, replay_prepopulate_steps)
+
+    # Initialize lists to store returns, lengths, and losses
+    rewards = []
+    returns = []
+    lengths = []
+    losses = []
+
+    # Initialize structures to store the models at different stages of training
+    t_saves = np.linspace(0, num_steps, num_saves - 1, endpoint=False)
+    saved_models = {}
+
+    i_episode = 0  # Use this to indicate the index of the current episode
+    t_episode = 0  # Use this to indicate the time-step inside current episode
+
+    state, _ = env.reset()  # Initialize state of first episode
+    G = 0
+
+    # Iterate for a total of `num_steps` steps
+    pbar = tqdm.trange(num_steps)
+    for t_total in pbar:
+        # Use t_total to indicate the time-step from the beginning of training
+
+        # Save model
+        if t_total in t_saves:
+            model_name = f"{100 * t_total / num_steps:04.1f}".replace(".", "_")
+            saved_models[model_name] = copy.deepcopy(dqn_model)
+
+        # YOUR CODE HERE:
+        #  * sample an action from the DQN using epsilon-greedy
+        #  * use the action to advance the environment by one step
+        #  * store the transition into the replay memory
+
+        action = (
+            env.action_space.sample()
+            if np.random.rand() < exploration.value(t_total)
+            else torch.argmax(
+                dqn_model(torch.tensor(state).float().unsqueeze(0))
+            ).item()
+        )
+
+        next_state, reward, done, _, _ = env.step(action)
+
+        memory.add(state, action, reward, next_state, done)
+        G = reward + gamma * G
+        rewards.append(reward)
+        # YOUR CODE HERE: Once every 4 steps,
+        #  * sample a batch from the replay memory
+        #  * perform a batch update (use the train_dqn_batch() method)
+
+        if t_total % 4 == 0:
+            batch = memory.sample(batch_size)
+            loss = train_dqn_batch(optimizer, batch, dqn_model, dqn_target, gamma)
+            losses.append(loss)
+
+        # YOUR CODE HERE: Once every 10_000 steps,
+        #  * update the target network (use the dqn_model.state_dict() and
+        #    dqn_target.load_state_dict() methods)
+
+        if t_total % 10_000 == 0:
+            dqn_target.load_state_dict(dqn_model.state_dict())
+
+        if done:
+            # YOUR CODE HERE: Anything you need to do at the end of an episode,
+            # e.g., compute return G, store returns/lengths,
+            # reset variables, indices, lists, etc.
+            # G = sum([gamma**i * rewards[i] for i in range(len(rewards))])
+            returns.append(G)
+            eps = exploration.value(t_total)
+            lengths.append(t_episode)
+
+            pbar.set_description(
+                f"Episode: {i_episode} | Steps: {t_episode + 1} | Return: {G:5.2f} | Epsilon: {eps:4.2f}"
+            )
+
+            state, _ = env.reset()
+            i_episode += 1
+            t_episode = 0
+            rewards = []
+            G = 0
+        else:
+            # YOUR CODE HERE: Anything you need to do within an episode
+            state = next_state
+            t_episode += 1
+
+    saved_models["100_0"] = copy.deepcopy(dqn_model)
+
+    return (
+        saved_models,
+        np.array(returns),
+        np.array(lengths),
+        np.array(losses),
+    )
